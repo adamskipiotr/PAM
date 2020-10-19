97efdf06006cce3d28a7644339a49a9655008413
adamskipiotr <adamski-piotrek@wp.pl>
2020-10-20 01:31:38 +0200

    Fast update: łączenie się z aplikacją backendową wystawioną na heroku

--- app/src/main/java/com/example/pam/MainActivity.kt
+++ app/src/main/java/com/example/pam/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.pam
 
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val url =  "https://pam-polsl.herokuapp.com/hello"
+        AsyncTaskHandleJson().execute(url)
 
         val registerActivityButton = findViewById<Button>(R.id.loginButton)
         registerActivityButton.setOnClickListener() {
@@ -23,5 +29,20 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java).apply {};
             startActivity(intent)
         }
+    }
+
+    inner class AsyncTaskHandleJson : AsyncTask<String, String, String>() {
+        override fun doInBackground(vararg url: String?): String {
+            var text:String
+            val connection = URL(url[0]).openConnection() as HttpURLConnection
+            try{
+                connection.connect()
+                text = connection.inputStream.use { it.reader().use { reader -> reader.readText() } }
+            }finally {
+                connection.disconnect()
+            }
+            return "OK";
+        }
+
     }
 }
